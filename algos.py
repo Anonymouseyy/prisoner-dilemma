f2bb977a1699893751f30b6c3161e338023d6395
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-04-25 21:05:36 -0700

    tit for tat and tit for two tats added

---
+++ algos.py
@@ -0,0 +1,12 @@
+def tit_for_tat(self_moves, opp_moves):
+    if not opp_moves:
+        return False
+
+    return opp_moves[-1]
+
+
+def tit_for_two_tats(self_moves, opp_moves):
+    if len(opp_moves) < 2:
+        return False
+
+    return opp_moves[-2] and opp_moves[-1]
