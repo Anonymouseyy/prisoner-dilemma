104129d45cca16f5fc4106ee9247873e28635787
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-05-02 23:01:51 -0700

    aggressive tit for tat

--- algos.py
+++ algos.py
@@ -17,6 +17,13 @@ def tit_for_tat(self_moves, opp_moves):
     return opp_moves[-1]
 
 
+def aggressive_tit_for_tat(self_moves, opp_moves):
+    if not opp_moves:
+        return True
+
+    return opp_moves[-1]
+
+
 def tit_for_two_tats(self_moves, opp_moves):
     if len(opp_moves) < 2:
         return False
